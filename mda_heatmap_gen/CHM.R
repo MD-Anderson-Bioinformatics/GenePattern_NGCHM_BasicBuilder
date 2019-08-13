4de2011e33d57b7d2a3a458fa3af535199f6f847
Ma <jma7@mdanderson.org>
2019-08-13 14:47:44 -0500

    change back R file

--- mda_heatmap_gen/CHM.R
+++ mda_heatmap_gen/CHM.R
@@ -17,8 +17,7 @@
 
 performDataOrdering<-function(dataFile, rowOrderMethod, rowDistanceMeasure, rowAgglomerationMethod, colOrderMethod, colDistanceMeasure, colAgglomerationMethod,rowOrderFile, colOrderFile, rowDendroFile, colDendroFile, rowCut, colCut)
 { 
-   print(dataFile)
-   dataMatrix = read.table(dataFile, header=TRUE, sep = "\t", check.names = FALSE, row.names = NULL, as.is=TRUE, na.strings=c("NA","N/A","-","?"))
+   dataMatrix = read.table(dataFile, header=TRUE, sep = "\t", check.names = FALSE, row.names = 1, as.is=TRUE, na.strings=c("NA","N/A","-","?"))
    rowOrder <-  createOrdering(dataMatrix, rowOrderMethod, "row", rowDistanceMeasure, rowAgglomerationMethod)  
    if (rowOrderMethod == "Hierarchical") {
       writeHCDataTSVs(rowOrder, rowDendroFile, rowOrderFile)
