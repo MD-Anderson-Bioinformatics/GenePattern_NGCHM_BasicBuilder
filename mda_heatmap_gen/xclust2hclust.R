44bf42c3139778556ac2c8e746174a0bc9d7be0c
Ma <jma7@mdanderson.org>
2020-02-14 17:11:00 -0600

    change the conversion of cdt file

--- mda_heatmap_gen/xclust2hclust.R
+++ mda_heatmap_gen/xclust2hclust.R
@@ -8,7 +8,7 @@ args = commandArgs(trailingOnly=TRUE)
 cdtFilePath = args [1]
 filePath = args[2]
 shaidyDir = args[3]
-result=xcluster2r(filePath,distance='euclidean')
+result=xcluster2r(filePath,distance='euclidean',labels=TRUE)
 ddg <- stats::as.dendrogram(result)
 shaidyInitRepository (shaidyDir, c("collection", "chm", "dataset", "dendrogram", "label", "tile", "viewer", "file"))
 shaidyRepo=shaidyLoadRepository ('file',shaidyDir)
@@ -16,8 +16,13 @@ ngchmSaveAsDendrogramBlob(shaidyRepo, ddg)
 
 tsvFilePath=paste(shaidyDir,"/matrix.tsv",sep="")
 if(!file.exists(tsvFilePath)){
-    data=read.csv(cdtFilePath,sep="\t",header=TRUE,row.names = 1)
-    data=data[,!(names(data) %in% c("Name","GWEIGHT"))]
-    data=data[!(row.names(data) %in% c("AID","EWEIGHT")),]
+    data=read.csv(cdtFilePath,sep="\t",header=TRUE)
+    # data=data[,!(names(data) %in% c("NAME","GWEIGHT","GID","Genename"))]
+    # data=data[!(row.names(data) %in% c("AID","EWEIGHT")),]
+    firstColumn=match(c("NA"),data[1,])+1
+    data=data[3:nrow(data),]
+    rownames=data$NAME    
+    data=data[,firstColumn:ncol(data)]
+    row.names(data)<-rownames
     write.table(data,tsvFilePath,quote=FALSE,sep="\t",col.names = NA)
 }
