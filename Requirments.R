a5ef20c5812ae2878493202f1e29584d677edf42
jma7 <jma7@mdanderson.org>
2020-02-20 10:51:16 -0600

    add Requirments.R

---
+++ Requirments.R
@@ -0,0 +1,3 @@
+install.packages(c('rjson','curl','devtools'),dependencies=TRUE, repos='http://cran.rstudio.com/')
+devtools::install_github('bmbroom/tsvio')
+devtools::install_github('bmbroom/NGCHMR')
