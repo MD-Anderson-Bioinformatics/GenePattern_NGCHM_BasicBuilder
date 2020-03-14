a1f4d2cc113c8e11f3f86593acd927c06b69fa3e
jma7 <jma7@mdanderson.org>
2020-03-13 22:15:32 -0500

    disable quoting when transform gct to tsv

--- mda_heatmap_gen/gctTotsv.R
+++ mda_heatmap_gen/gctTotsv.R
@@ -2,6 +2,6 @@
 args = commandArgs(trailingOnly=TRUE)
 gctFile=args [1]
 tsvFilePath= gsub(".gct",".tsv",gctFile)
-gctFile<-read.csv(gctFile,sep="\t",header=TRUE,row.names=1,skip=2)
+gctFile<-read.csv(gctFile,sep="\t",header=TRUE,row.names=1,quote="",stringsAsFactors=FALSE,skip=2)
 gctFile=gctFile[, !(colnames(gctFile) %in% c("Description"))]
 write.table(gctFile,tsvFilePath,quote=FALSE,sep="\t",col.names = NA)
