208bcf0c84fb97e81b0dbca7a9bd5ea6eba9cfee
jma7 <jma7@mdanderson.org>
2020-04-18 15:47:42 -0500

    update example cdt, atr and gtr

--- mda_heatmap_gen/xcluster2r.R
+++ mda_heatmap_gen/xcluster2r.R
@@ -77,8 +77,9 @@ if(substr(distance,1,1)=="e")
     # median_number=median(data[,4])
     # factor=1/median_number
     # Hheight <- 2 / (data[,4]*factor+0.1)   #Distance = Euclidean 
-    Hheight <- 1 / (data[,4]+0.01)
-    Hheight <- Hheight-min(Hheight)+0.1
+     Hheight <- 2 / (data[,4]+0.1)   #Distance = Euclidean 
+    # Hheight <- 1 / (data[,4]+0.01)
+    # Hheight <- Hheight-min(Hheight)
    }
    else{
         Hheight <- 1-data[,4]           #Distance = pearson (centered or not)
